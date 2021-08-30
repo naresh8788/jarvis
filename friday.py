7f9b740a1c558db106e9c185eaacfccb453bb4fd
naresh8788 <89772602+naresh8788@users.noreply.github.com>
2021-08-30 14:01:02 +0530

    Add files via upload

---
+++ friday.py
@@ -0,0 +1,14 @@
+import pyttsx3
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+print(voices[0].id)
+engine.setProperty('voices', voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    print(audio)
+    engine.runAndWait()
+
+if__name__ == '__main__';
+speak('hello sir')
+
